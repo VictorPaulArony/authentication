b3f7557a376ee3a9052d616dab8e482e019409eb
victorpaularony <smallypauls@gmail.com>
2025-08-27 21:48:03 +0300

    Update TeacherRegisterRequest.java: Refactor teacher register request DTO

--- src/main/java/com/example/authentication/dto/TeacherRegisterRequest.java
+++ src/main/java/com/example/authentication/dto/TeacherRegisterRequest.java
@@ -1,8 +1,14 @@
 package com.example.authentication.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class TeacherRegisterRequest {
     private String username;
     private String password;
