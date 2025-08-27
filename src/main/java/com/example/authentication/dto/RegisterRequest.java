cd6ad1f31168df4e180fa95025ee90c82cf3c63a
victorpaularony <smallypauls@gmail.com>
2025-08-27 21:48:01 +0300

    Update RegisterRequest.java: Refactor register request DTO

--- src/main/java/com/example/authentication/dto/RegisterRequest.java
+++ src/main/java/com/example/authentication/dto/RegisterRequest.java
@@ -1,10 +1,14 @@
 package com.example.authentication.dto;
 
-// import lombok.Data;
-
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class RegisterRequest {
     private String username;
     private String password;
